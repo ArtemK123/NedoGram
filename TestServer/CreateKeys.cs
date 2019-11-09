7dfb476826993f99f3c64bd33cbc8894a75797b2
Artem Kuchma <darkmode1012@gmail.com>
2019-11-09 07:35:13 +0200

    Folder struture refactored

---
+++ TestServer/CreateKeys.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace CreateKeys
+{
+    class CreateKeys
+    {
+        static void Create()
+        {
+            using (var aesAlg = new AesManaged())
+            {
+                aesAlg.GenerateKey();
+                aesAlg.GenerateIV();
+                using (var keyWriter = new StreamWriter("D:/WpfChat/key.txt"))
+                {
+                    Console.WriteLine("Used Key for Encryption: " + BitConverter.ToString(aesAlg.Key));
+                    keyWriter.WriteLine(BitConverter.ToString(aesAlg.Key));
+                }
+                using (var IvWriter = new StreamWriter("D:/WpfChat/iv.txt"))
+                {
+                    Console.WriteLine("Used IV for Encryption: " + BitConverter.ToString(aesAlg.IV));
+                    IvWriter.WriteLine(BitConverter.ToString(aesAlg.IV));
+                }
+            }
+        }
+    }
+}
---
+++ TestServer/Program.cs
@@ -0,0 +1,37 @@
+﻿using ChatCommon;
+using ChatCommon.Extensibility;
+using System;
+using System.Text;
+
+namespace TestServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IEncryption encryption = new AesEncryption();
+            //ICoding unicode = new Coding(new UnicodeEncoding(false, false, true));
+            //string text = "hello world";
+
+            //byte[] encrypted = encryption.Encrypt(text);
+            //byte[] encoded = unicode.Encode(text);
+
+            //StringBuilder encryptedBuilder = new StringBuilder();
+            //StringBuilder encodedBuilder = new StringBuilder();
+
+            //foreach(var b in encrypted)
+            //{
+            //    encryptedBuilder.Append(b);
+            //}
+            //foreach(var b in encoded)
+            //{
+            //    encodedBuilder.Append(b);
+            //}
+
+            //Console.WriteLine($"Encrypted bytes: {encryptedBuilder.ToString()}");
+            //Console.WriteLine($"Encoded bytes: {encodedBuilder.ToString()}");
+
+            Console.ReadKey();
+        }
+    }
+}
