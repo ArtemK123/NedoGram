09dfec2001d1101b843d5664b8473991e595e15a
Artem Kuchma <artem.kuchma@sigma.software>
2019-11-16 14:30:46 +0200

    AES uses stream handler

--- ChatCommon/Encryption/AesEncryption.cs
+++ ChatCommon/Encryption/AesEncryption.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using System.Security.Cryptography;
-using System.Text;
 using ChatCommon.Extensibility;
 
 namespace ChatCommon.Encryption
@@ -10,9 +7,11 @@ namespace ChatCommon.Encryption
     public class AesEncryption : IEncryption
     {
         private readonly AesManaged aesNative;
+        private readonly IStreamHandler streamHandler;
 
         public AesEncryption(byte[] key = null, byte[] iv = null)
         {
+            streamHandler = new StreamHandler();
             aesNative = new AesManaged();
             if (key != null)
             {
@@ -27,78 +26,30 @@ namespace ChatCommon.Encryption
 
         public byte[] Encrypt(byte[] plainTextBytes)
         {
-            byte[] encrypted = new byte[plainTextBytes.Length];
-
-            byte[] tempData = new byte[plainTextBytes.Length];
-
-            // Create an AesManaged object
-            // with the specified key and IV.
-
-            // Create an encryptor to perform the stream transform.
             ICryptoTransform encryptor = aesNative.CreateEncryptor(aesNative.Key, aesNative.IV);
 
-
-            using (var input = new MemoryStream(plainTextBytes))
             using (var output = new MemoryStream())
             {
                 using (var cryptStream = new CryptoStream(output, encryptor, CryptoStreamMode.Write))
                 {
-                    var buffer = new byte[64];
-                    var read = input.Read(buffer, 0, buffer.Length);
-                    while (read > 0)
-                    {
-                        cryptStream.Write(buffer, 0, read);
-                        read = input.Read(buffer, 0, buffer.Length);
-                    }
-                    cryptStream.FlushFinalBlock();
-                    encrypted = output.ToArray();
+                    streamHandler.Write(cryptStream, plainTextBytes, 64);
+                    return output.ToArray();
                 }
             }
-
-            // Create the streams used for encryption.
-            //using (MemoryStream msEncrypt = new MemoryStream())
-            //{
-            //    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
-            //    {
-            //        csEncrypt.Write(plainTextBytes);
-            //    }
-
-            //    encrypted = msEncrypt.ToArray();
-            //}
-
-            return encrypted;
         }
 
         public byte[] Decrypt(byte[] encryptedText)
         {
-            // Declare the byte[] used to hold
-            // the decrypted text.
-            List<byte> decrypted = new List<byte>();
-            
-            // Create a decryptor to perform the stream transform.
             ICryptoTransform decryptor = aesNative.CreateDecryptor(aesNative.Key, aesNative.IV);
 
-            // Create the streams used for decryption.
             using (MemoryStream msDecrypt = new MemoryStream(encryptedText))
             {
                 using (CryptoStream cryptoStream = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
                 {
-                    // todo: create stream reader and move this logic there
-                    
-                    var buffer = new byte[1024];
-                    var read = cryptoStream.Read(buffer, 0, buffer.Length);
-                    while (read > 0)
-                    {
-                        decrypted.AddRange(buffer.Take(read));
-                        read = cryptoStream.Read(buffer, 0, buffer.Length);
-                    }
-                    cryptoStream.Flush();
+                    return streamHandler.Read(cryptoStream);
                 }
             }
-
-            return decrypted.ToArray();
         }
-
 
         public byte[] GetKey() => aesNative.Key;
 
