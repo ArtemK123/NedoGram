924873bf1cbf8813fa3dcb08685ced08d4f6a881
Artem Kuchma <artem.kuchma@sigma.software>
2019-11-16 14:22:46 +0200

    StreamHandler added

---
+++ ChatCommon/Extensibility/IStreamHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+
+namespace ChatCommon.Extensibility
+{
+    public interface IStreamHandler
+    {
+        byte[] Read(Stream stream, int bufferSize = 64);
+
+        void Write(Stream stream, byte[] bytesToWrite, int bufferSize = 64);
+    }
+}
---
+++ ChatCommon/StreamHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ChatCommon.Extensibility;
+
+namespace ChatCommon
+{
+    public class StreamHandler : IStreamHandler
+    {
+        public byte[] Read(Stream stream, int bufferSize = 64)
+        {
+            var buffer = new byte[bufferSize];
+            List<byte> bytes = new List<byte>();
+            var read = stream.Read(buffer, 0, buffer.Length);
+            while (read > 0)
+            {
+                bytes.AddRange(buffer.Take(read));
+                read = stream.Read(buffer, 0, buffer.Length);
+            }
+            stream.Flush();
+
+            return bytes.ToArray();
+        }
+
+        public void Write(Stream stream, byte[] bytesToWrite, int bufferSize = 64)
+        {
+            using (var tempStream = new MemoryStream(bytesToWrite))
+            {
+                var buffer = new byte[64];
+                var read = tempStream.Read(buffer, 0, buffer.Length);
+                while (read > 0)
+                {
+                    stream.Write(buffer, 0, read);
+                    read = tempStream.Read(buffer, 0, buffer.Length);
+                }
+
+                stream.Flush();
+            }
+        }
+    }
+}
